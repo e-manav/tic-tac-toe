4359cd141f396c3514c5706ae33c55e9233cfd5b
Vidhan Gupta <vidhangupta094@gmail.com>
2024-08-01 20:50:00 +0530

    add onclick handler in board

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
-export default function Square() {
-  return <button className="square">V</button>;
+function Square({ value }) {
+  function handleClick() {
+    console.log("clicked!");
+  }
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1"></Square>
+        <Square value="2"></Square>
+        <Square value="3"></Square>
+      </div>
+      <div className="board-row">
+        <Square value="4"></Square>
+        <Square value="5"></Square>
+        <Square value="6"></Square>
+      </div>
+      <div className="board-row">
+        <Square value="7"></Square>
+        <Square value="8"></Square>
+        <Square value="9"></Square>
+      </div>
+    </>
+  );
 }
